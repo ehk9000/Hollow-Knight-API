17b2d0a255513216d50d42f72b4d06f6fdfc18e9
Erik Konner <erikkonner@gmail.com>
2019-06-25 23:18:56 -0600

    promises for creating both tables and their rows

--- db/migrations/20190625224321_initial.js
+++ db/migrations/20190625224321_initial.js
@@ -1,8 +1,29 @@
 
 exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('Friendly NPC', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.json('image');
+
+      table.timestamps(true, true);
+    }),
+
+    knex.schema.createTable('Bosses', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.json('image');
+
+      table.timestamps(true, true);
+    })
+  ])
   
 };
 
 exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('Friendly NPC'),
+    knex.schema.dropTable('Bosses')
+  ])
   
 };
