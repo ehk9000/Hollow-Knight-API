19d81a40c76c111c6e6401ef873ea73c082aebf7
Erik Konner <erikkonner@gmail.com>
2019-06-25 23:40:18 -0600

    Initialized app file. get method for bosses

---
+++ app.js
@@ -0,0 +1,23 @@
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log(`App is running ⚡️ on port ${port}`)
+});
+
+app.get('/api/v1/hollowknight/bosses', (request, response) => {
+  database('bosses').select()
+    .then((bosses) => {
+      response.status(200).json(bosses);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    })
+});
+
