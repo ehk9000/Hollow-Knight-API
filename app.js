39b3bf316c073da5d0c6e3a1208e1b20a706d6ad
Erik Konner <erikkonner@gmail.com>
2019-06-28 10:26:15 -0600

    troubleshooting deployment

--- app.js
+++ app.js
@@ -8,40 +8,41 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.json());
 
-app.listen(app.get('port'), () => {
-  console.log(`App is running ⚡️`)
-});
 
 app.get('/', (req, res) => {
-    res.status(200).json('Hello world!');
+  res.status(200).json('Hello world!');
 });
 
 app.get('/api/v1/hollow-knight/bosses', (request, response) => {
   database('bosses').select()
-    .then((bosses) => {
-      response.status(200).json(bosses);
-    })
-    .catch((error) => {
-      response.status(500).json({ error });
-    })
+  .then((bosses) => {
+    response.status(200).json(bosses);
+  })
+  .catch((error) => {
+    response.status(500).json({ error });
+  })
 });
 
 app.get('/api/v1/hollow-knight/locations', (request, response) => {
   database('locations').select()
-    .then((locations) => {
-      response.status(200).json(locations)
-    })
-    .catch((error) => {
-      response.status(500).json({ error });
-    })
+  .then((locations) => {
+    response.status(200).json(locations)
+  })
+  .catch((error) => {
+    response.status(500).json({ error });
+  })
 });
 
 app.get('/api/v1/hollow-knight/friendly-npcs', (request, response) => {
   database('friendly npcs').select()
-    .then((npcs) => {
-      response.status(200).json(npcs)
-    })
-    .catch((error) => {
-      response.status(500).json({ error })
-    })
+  .then((npcs) => {
+    response.status(200).json(npcs)
+  })
+  .catch((error) => {
+    response.status(500).json({ error })
+  })
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`App is running ⚡️`)
 });
