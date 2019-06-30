4ccd60f33cb07412ab1c72e5227bf9f1ce706770
Erik Konner <erikkonner@gmail.com>
2019-06-30 14:01:33 -0600

    post methods for npcs and bosses

--- app.js
+++ app.js
@@ -53,17 +53,56 @@ app.post('/api/v1/hollow-knight/bosses', (request, response) => {
     if (!boss[requiredParameter]) {
       return response
         .status(422)
-        .send({ error: `Expected format: {name: <String>, image: <String>,
-           location: <String>}. You are missing "${requiredParameter}" property.`})
-
+        .send({ error: `Expected format: {name: <String>, image: <String>, location:<String> 
+          You are missing "${requiredParameter}" property.`})
     }
   }
-  database('bosses').insert(boss, 'id')
-    .then(boss =>  {
-      response.status(201).json({id: boss[0]})
-    })
-    .catch(error => {
-      response.status(500).json({ error })
+
+  let newBoss = {
+    name: boss.name,
+    image: boss.image
+  }
+
+  database('locations').where({name: boss.location}).select('id')
+    .then(locationId => {
+      database('bosses').insert({...newBoss, location_id: locationId[0].id}, 'id')
+        .then(boss => {
+          response.status(201).json({ id: boss[0] })
+        })
+        .catch(error => {
+          response.status(500).json({ error })
+        })
     })
 });
+
+app.post('/api/v1/hollow-knight/friendly-npcs', (request, response) => {
+  const npc = request.body;
+
+  for (let requiredParameter of ['name', 'image', 'location']) {
+    if (!npc[requiredParameter]) {
+      return response 
+        .status(422)
+        .send({ error: `Expected format: {name: <String>, image: <String>, location: <String>}
+          You are missing "${requiredParameter}" property.`})
+    }
+  }
+
+  let newNpc = {
+    name: npc.name,
+    image: npc.image
+  }
+
+  database('locations').where({name: npc.location}).select('id')
+    .then(locationId => {
+      database('friendly npcs').insert({...newNpc, location_id: locationId[0].id}, 'id')
+      .then(npc => {
+        response.status(201).json({ id: npc[0] })
+      })
+      .catch(error => {
+        response.status(500).json({ error })
+      })
+    })
+})
+
+// app.get('/api/v1/hollow-knight/bosses:id')``
 
